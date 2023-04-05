8038d44ef5716e231337c1e4c577c483d3921ae2
licht1stein <mb@m1k.pw>
2023-04-05 21:02:29 +0100

    Get rid of multiple arities

--- src/bblgum/core.clj
+++ src/bblgum/core.clj
@@ -1,38 +1,6 @@
 (ns bblgum.core
   (:require [bblgum.impl :as i]))
 
-(defn- gum*
-  "Low level API. If you don't know why you are using `gum*` then you're probably looking for `gum`
-
-  Options map:
-  cmd: The interaction command. Can be a keyword. Required
-  opts: A map of options to be passed as optional params to gum
-  args: A vec of args to be passed as positional params to gum
-  in: An input stream than can be passed to gum
-  as: Coerce the output. Currently supports :bool, :ignored or defaults to a seq of strings
-  gum-path: Path to the gum binary. Defaults to gum
-
-  Returns a map of:
-  status: The exit code from gum
-  result: The output from the execution: seq of lines or coerced via :as."
-  [{:keys [cmd opts args in as gum-path]}]
-  (when-not cmd
-    (throw (IllegalArgumentException. ":cmd must be provided or non-nil")))
-  (let [gum-path (or gum-path "gum")
-        with-opts (->> opts
-                       (map (fn [[opt value]]
-                              (str "--" (i/->str opt) "=" (i/multi-opt value))))
-                       (into [gum-path (i/->str cmd)]))
-        out (if (= :ignored as) :inherit :string)
-        args (if (or (empty? args) (= "--" (first args)))
-               args
-               (cons "--" args))
-        {:keys [exit out]} (i/exec (into with-opts args) in out)]
-    {:status exit
-     :result (case as
-               :bool (zero? exit)
-               out)}))
-
 (defn gum
   "High level gum API. Simpler in usage then `gum*`, but uses it under the hood.
 
@@ -65,10 +33,21 @@
   Returns a map of:
   status: The exit code from gum
   result: The output from the execution: seq of lines or coerced via :as."
-  ([cmd]
-   (gum* (i/prepare-cmd-map cmd)))
-  ([cmd args-or-opts]
-   (gum* (i/prepare-cmd-map cmd args-or-opts)))
-  ([cmd args & opts]
-   (gum* (apply i/prepare-cmd-map cmd args opts))))
-
+  [cmd & [args & opts]]
+  (let [{:keys [cmd opts args in as gum-path]} (apply i/prepare-cmd-map cmd args opts)]
+    (when-not cmd
+      (throw (IllegalArgumentException. ":cmd must be provided or non-nil")))
+    (let [gum-path (or gum-path "gum")
+          with-opts (->> opts
+                         (map (fn [[opt value]]
+                                (str "--" (i/->str opt) "=" (i/multi-opt value))))
+                         (into [gum-path (i/->str cmd)]))
+          out (if (= :ignored as) :inherit :string)
+          args (if (or (empty? args) (= "--" (first args)))
+                 args
+                 (cons "--" args))
+          {:keys [exit out]} (i/exec (into with-opts args) in out)]
+      {:status exit
+       :result (case as
+                 :bool (zero? exit)
+                 out)})))
